e0c4265a8be0d90c790aae9b58548c7421a516ff
Luke Todd <lukejamestodd1@gmail.com>
2016-01-13 16:55:11 +1100

    shelter class working. no adoptions yet

--- main.rb
+++ main.rb
@@ -1,52 +1,13 @@
 require './client.rb'
 require './animal.rb'
-#require './shelter.rb'
+require './shelter.rb'
 require 'pry'
 
-#to store objects
+#to store objects - test cases are done in main first
 animalsArray = []
 clientsArray = []
 
-#to create new animal with user input
-def inputNewAnimal(animalsArray)
-		puts 'enter the new animal name:'
-		name = gets.chomp
-		puts 'enter age: '
-		age = gets.chomp
-		age = age.to_i
-		puts 'enter gender: '
-		gender = gets.chomp
-		puts 'enter species: '
-		species = gets.chomp
-		puts 'enter toys (separated by a space): '
-		toys = gets.chomp
-		toys = toys.split(' ')
-
-		newAnimal = Animal.new(name, age, gender, species, toys)
-		animalsArray.push(newAnimal)
-		return animalsArray
-end
-
-#create new client from user input
-def inputNewClient(clientsArray)
-		puts 'enter the new client name:'
-		name = gets.chomp
-		puts 'enter number of children: '
-		no_of_children = gets.chomp
-		no_of_children = no_of_children.to_i
-		puts 'enter age: '
-		age = gets.chomp
-		age = age.to_i
-		puts 'enter number of other pets: '
-		no_of_pets = gets.chomp
-		no_of_pets = no_of_pets.to_i
-
-		newClient = Client.new(name, no_of_children, age, no_of_pets)
-		clientsArray.push(newClient)
-		return clientsArray
-end
-
-#test cases - new objects pushed to arrays
+#test cases - new objects pushed straight to above arrays
 client1 = Client.new('patty', 3, 56, 2)
 client2 = Client.new('betty davis', 4, 22, 3)
 animal1 = Animal.new('Felix', 2, 'M', 'cat', ['ball', 'string'])
@@ -54,29 +15,35 @@ animal2 = Animal.new('Lassie', 6, 'F', 'dog', ['frisbee', 'bone'])
 animalsArray.push(animal1, animal2)
 clientsArray.push(client1, client2)
 
+#new shelter object, pas both arrays in
+fancyShelter = Shelter.new(animalsArray, clientsArray)
+
+#main menu
 selection = "0"
-while selection != "5"
+while selection != "7"
 	#main menu
 	puts 'Welcome to the animal shelter. What would you like to do?'
 	puts '1 - Add new animal'
 	puts '2 - Add new client'
-	puts '3 - Print animals DB'
-	puts '4 - Print clients DB'
-	puts '5 - Exit'
+	puts '3 - Print animals database'
+	puts '4 - Print clients database'
+	puts '5 - Enter an adoption'
+	puts '6 - Enter an abandoned pet'
+	puts '7 - Exit'
 	case selection
 		when "1"
-			inputNewAnimal(animalsArray)
+			fancyShelter.inputNewAnimal
 		when "2"
-			inputNewClient(clientsArray)
+			fancyShelter.inputNewClient
 		when "3"
-			animalsArray.each do |animal|
-		  	puts "#{animal.name}  #{animal.age}  #{animal.gender}  #{animal.species}"
-			end 
+			fancyShelter.printAnimals
 		when "4"
-			clientsArray.each do |client|
-		  	puts "#{client.name}  #{client.no_of_children}  #{client.age}  #{client.no_of_pets}"
-			end
-		end
+			fancyShelter.printClients 
+		when "5"
+			fancyShelter.adoptPet
+		when "6"
+			fancyShelter.abandonPet
+	end
 		selection = gets.chomp
 end 
 
--- shelter.rb
+++ shelter.rb
@@ -1,14 +1,61 @@
 require './client.rb'
 require './animal.rb'
-require './main.rb'
 
 class Shelter
-
-	attr_accessor :animals, :clients
+	attr_accessor :animalsArray, :clientsArray
 	
-	def initialize(animals, clients)
-		@clients = clients
-		@animals = animals
+	def initialize(animalsArray, clientsArray)
+		@clientsArray = clientsArray
+		@animalsArray = animalsArray
+	end
+
+	#to create new animal with user input
+	def inputNewAnimal
+			puts 'enter the new animal name:'
+			name = gets.chomp
+			puts 'enter age: '
+			age = gets.chomp
+			age = age.to_i
+			puts 'enter gender: '
+			gender = gets.chomp
+			puts 'enter species: '
+			species = gets.chomp
+			puts 'enter toys (separated by a space): '
+			toys = gets.chomp
+			toys = toys.split(' ')
+
+			newAnimal = Animal.new(name, age, gender, species, toys)
+			@animalsArray.push(newAnimal)
+	end
+
+	#create new client from user input
+	def inputNewClient
+			puts 'enter the new client name:'
+			name = gets.chomp
+			puts 'enter number of children: '
+			no_of_children = gets.chomp
+			no_of_children = no_of_children.to_i
+			puts 'enter age: '
+			age = gets.chomp
+			age = age.to_i
+			puts 'enter number of other pets: '
+			no_of_pets = gets.chomp
+			no_of_pets = no_of_pets.to_i
+
+			newClient = Client.new(name, no_of_children, age, no_of_pets)
+			@clientsArray.push(newClient)
+	end
+
+	def printAnimals
+		@animalsArray.each do |animal|
+		  puts "#{animal.name}  #{animal.age}  #{animal.gender}  #{animal.species}"
+		end 	
+	end 
+
+	def printClients
+		@clientsArray.each do |client|
+		  	puts "#{client.name}  #{client.no_of_children}  #{client.age}  #{client.no_of_pets}"
+		end 
 	end
 
 	def adopt(client, animal)
@@ -17,4 +64,11 @@ class Shelter
 	def abandon(client, animal)
 	end 
 
-end 
+
+
+end
+
+
+
+
+
